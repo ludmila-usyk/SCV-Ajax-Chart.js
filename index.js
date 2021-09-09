c89ca8c013994fdede3f95fa7bf1692b909738b4
milausikk <milausik@ukr.net>
2021-09-09 10:59:23 +0300

    added Northern/Southern temp

--- index.js
+++ index.js
@@ -14,9 +14,11 @@ function fetchData() {
             const mappedData = parsedData.reduce((acc, entry) => {
                 acc.years.push(entry.Year);
                 acc.temps.push(Number(entry.Glob) + GLOBAL_MEAN_TEMPERATURE);
+                acc.tempN.push(Number(entry.NHem) + GLOBAL_MEAN_TEMPERATURE);
+                acc.tempS.push(Number(entry.SHem) + GLOBAL_MEAN_TEMPERATURE);
 
                 return acc;
-            }, {years: [], temps: []});
+            }, {years: [], temps: [], tempN: [], tempS: []});
             // const years = parsedData.map(entry => entry.Year);
             // const temps = parsedData.map(entry => Number(entry.Glob) + GLOBAL_MEAN_TEMPERATURE);
             
@@ -24,13 +26,29 @@ function fetchData() {
     type: 'line',
     data: {
         labels: mappedData.years,
-        datasets: [{
+        datasets: [
+            {
             label: '# GLOBAL TEMPERATURE',
             data: mappedData.temps,
             borderColor: "rgba(255, 99, 132, 1)",
             borderWidth: 1,
             fill: false,
-        }]
+            },
+            {
+            label: '# Northern Hemisphere TEMPERATURE',
+            data: mappedData.tempN,
+            borderColor: "rgba(205, 129, 67, 1)",
+            borderWidth: 1,
+            fill: false,
+            },
+            {
+            label: '# Southern Hemisphere TEMPERATURE',
+            data: mappedData.tempS,
+            borderColor: "rgba(16, 44, 9, 1)",
+            borderWidth: 1,
+            fill: false,
+            }
+        ]
     },
     options: {
         scales: {
@@ -41,6 +59,13 @@ function fetchData() {
                     }
                 }
             }
+            // x: {
+            //     ticks: {
+            //         callback(value) {
+            //             return value + 'years';
+            //         }
+            //     }
+            // }
         }
     }
 });
